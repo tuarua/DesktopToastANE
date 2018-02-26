3946e596d8238d03635b1abee3f9f28675f7b65e
Eoin Landy <el111@yahoo.com>
2018-02-26 21:43:56 +0000

    github downloads needs TLS1.2

--- example/get_dependencies.ps1
+++ example/get_dependencies.ps1
@@ -1,3 +1,2 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
-
 Invoke-WebRequest -Uri https://github.com/tuarua/Swift-OSX-ANE/releases/download/2.2.0/CommonDependencies.ane?raw=true -OutFile "$currentDir\..\native_extension/ane/CommonDependencies.ane"
